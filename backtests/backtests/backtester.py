181e34ebeb2a3945a8bd5eced0be353c2bec77fc
raularmu89-stack <raul_armu_89@hotmail.com>
2026-03-16 20:08:40 +0100

    Add strategy parameter to Backtester constructor

--- backtests/backtests/backtester.py
+++ backtests/backtests/backtester.py
@@ -1,7 +1,12 @@
-
 class Backtester:
-    def __init__(self, data):
+    def __init__(self, strategy, data):
+        self.strategy = strategy
         self.data = data
 
     def run(self):
         print("Ejecutando backtest...")
+        return {
+            "trades": 0,
+            "winrate": 0,
+            "profit_factor": 0
+        }
