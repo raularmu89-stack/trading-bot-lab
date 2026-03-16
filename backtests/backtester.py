5755fc2cfddfa1fc5df45bbac59d74cbdef13704
raularmu89-stack <raul_armu_89@hotmail.com>
2026-03-16 21:01:38 +0100

    Update backtester.py

--- backtests/backtester.py
+++ backtests/backtester.py
@@ -1,1 +1,17 @@
+class Backtester:
+    def __init__(self, strategy, data):
+        self.strategy = strategy
+        self.data = data
 
+    def run(self):
+        signal = self.strategy.generate_signal(self.data)
+
+        print("Backtest result:")
+        print(signal)
+
+        return {
+            "trades": 1 if signal["signal"] in ["buy", "sell"] else 0,
+            "winrate": 0,
+            "profit_factor": 0,
+            "signal": signal
+        }
