49d88e934bb06839f6a08ec1e8bc5461d82daaa6
raularmu89-stack <raul_armu_89@hotmail.com>
2026-03-16 20:07:15 +0100

    Add market structure detection function

--- indicators/indicadores/indicadores/market_structure.py
+++ indicators/indicadores/indicadores/market_structure.py
@@ -1,1 +1,9 @@
+def detect_market_structure(data):
+    if data is None or len(data) < 3:
+        return None
 
+    return {
+        "trend": "neutral",
+        "bos": False,
+        "choch": False
+    }
